0aa8f3068c1bccb805bed72e176a734fcc440c2e
Robin <roleciq@gmail.com>
2021-10-18 23:25:38 -0400

    newspend fragment created

---
+++ app/src/main/java/com/mobile/sharedwallet/fragment/NewSpendFragment.kt
@@ -0,0 +1,18 @@
+package com.mobile.sharedwallet.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mobile.sharedwallet.R
+
+class NewSpendFragment: Fragment() {
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val view: View = inflater.inflate(R.layout.newspend_fragment, container, false)
+        return view
+    }
+}
