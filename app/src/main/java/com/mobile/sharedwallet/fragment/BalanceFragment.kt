2eb99b32377c9df971be43baccff56adf0381d92
ORIOL <rems.oriol@gmail.com>
2021-11-08 17:53:53 -0500

    graphique marche, pb avec le solde

--- app/src/main/java/com/mobile/sharedwallet/fragment/BalanceFragment.kt
+++ app/src/main/java/com/mobile/sharedwallet/fragment/BalanceFragment.kt
@@ -3,12 +3,21 @@ package com.mobile.sharedwallet.fragment
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mobile.sharedwallet.R
 import com.mobile.sharedwallet.dialog.AddUserToPotDialog
@@ -18,16 +27,67 @@ import com.mobile.sharedwallet.utils.Utils
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.text.DecimalFormat
 import kotlin.math.abs
 import kotlin.math.min
 
 class BalanceFragment: Fragment() {
 
     private var participants : ArrayList<Participant>? = null
+    private var partifForGraph : MutableList<Participant>? = (CagnotteFragment.pot.participants)?.toMutableList()
+
+    class ChartXAxisFormatter() : ValueFormatter(), Parcelable {
+
+
+        constructor(a:ArrayList<String>) :this(){
+            names = a.toTypedArray()
+        }
+        private var names = arrayOf("Julien", "Robin", "Remi")
+
+        constructor(parcel: Parcel) : this() {
+            names = parcel.createStringArray() as Array<String>
+        }
+
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            return names.getOrNull(value.toInt()) ?: value.toString()
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+            parcel.writeStringArray(names)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<ChartXAxisFormatter> {
+            override fun createFromParcel(parcel: Parcel): ChartXAxisFormatter {
+                return ChartXAxisFormatter(parcel)
+            }
+
+            override fun newArray(size: Int): Array<ChartXAxisFormatter?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
+
+    class ChartYValueFormatter : ValueFormatter() {
+
+        private val format = DecimalFormat("###.#")
+
+        // override this for BarChart
+        override fun getBarLabel(barEntry: BarEntry?): String {
+            return format.format(barEntry?.y)
+        }
+
+        // override this for custom formatting of XAxis or YAxis labels
+        override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+            return format.format(value)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        participants = CagnotteFragment.pot.participants
     }
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -43,7 +103,9 @@ class BalanceFragment: Fragment() {
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        participants = CagnotteFragment.pot.participants
         quiDoitQuoi()
+        DisplayGraph()
     }
 
     private fun quiDoitQuoi(){
@@ -83,5 +145,64 @@ class BalanceFragment: Fragment() {
         newTextView.textSize = 15f
         newTextView.text = "$p2 owes $debt to $p1"
         liste?.addView(newTextView)
+    }
+
+    private fun GatherInfo() : ArrayList<BarEntry>{
+        val entries: ArrayList<BarEntry> = ArrayList()
+        var i = 0f
+        partifForGraph?.forEach{
+            entries.add(BarEntry(i, it.solde))
+            //axeAbs.names?.add(it.name)
+            println(it.solde)
+            println(it.name)
+            i += 1.0f
+        }
+        entries.clear()
+        entries.add(BarEntry(0f, 85f))
+        entries.add(BarEntry(1f, 10.5f))
+        entries.add(BarEntry(2f, -24.6f))
+
+        return entries
+    }
+
+    private fun DisplayGraph(){
+        val entries = GatherInfo()
+        var names: ArrayList<String> = ArrayList()
+        partifForGraph?.forEach {
+            names.add(it.name)
+        }
+
+        var axeAbs = ChartXAxisFormatter(names)
+        var axeOrd = ChartYValueFormatter()
+
+        val barDataSet = BarDataSet(entries, "Graph of the costs")
+        barDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+
+        var barChart = view?.findViewById<BarChart>(R.id.barChart)
+        val data = BarData(barDataSet)
+        if (barChart != null) {
+            barChart.data = data
+        }
+        else {
+            println("barchart null")
+        }
+
+        if (barChart != null) {
+            barChart.axisLeft.setDrawGridLines(false)
+            barChart.xAxis.setDrawGridLines(false)
+            barChart.xAxis.setDrawAxisLine(false)
+            barChart.xAxis.granularity = 1.0f
+            barChart.xAxis.valueFormatter = axeAbs
+            barChart.axisLeft.valueFormatter = axeOrd
+            //remove right y-axis
+            barChart.axisRight.isEnabled = false
+            //remove legend
+            barChart.legend.isEnabled = false
+            //remove description label
+            barChart.description.isEnabled = false
+            //add animation
+            barChart.animateY(1000)
+            barChart.invalidate()
+        }
     }
 }
